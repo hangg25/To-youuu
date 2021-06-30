c15ac27ecb4f41c815770090c5288f1187222328
hangg25 <86718191+hangg25@users.noreply.github.com>
2021-06-30 17:48:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,14 +3,14 @@ const CONFIG = {
     introDesc: `Tớ có điều muốn hỏi cậu trả lời thật lòng nhaaa`,
     btnIntro: 'hihi',
     title: 'Cậu thích tớ có phải hum nàoo 😙',
-    desc: 'Không trả lời mà thoát ra là muốn làm người yêu tớ rồi',
+    desc: 'Không trả lời mà thoát ra là muốn làm người yêu tớ rồi kiki',
     btnYes: 'Yessss 💞',
     btnNo: 'Noooo 😢',
     question:'Tại sao lại thích tớ théee 😆',
-    btnReply: '',
+    btnReply: 'Aa',
     reply: 'Thích thì đâu cần lí do đâu nhỉ',
-    mess: 'I love u too 🤗',
+    mess: '🥳',
     messDesc: 'Hẹn cậu một ngày nào đó đi chơi với tớ nhaaa 🛵',
     btnAccept: 'Okii nhaa',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://fb.com/messages/t/100013251076887/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
